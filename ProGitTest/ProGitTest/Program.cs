575e2549fbf149b8acd39bcf98aeca1746f43200
subhankar.M <subhoconnect@hotmail.com>
2022-06-28 12:21:07 +0530

    My 2nd commit

--- ProGitTest/ProGitTest/Program.cs
+++ ProGitTest/ProGitTest/Program.cs
@@ -6,7 +6,7 @@ namespace ProGitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is just a test");
         }
     }
 }
